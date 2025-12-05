deeb250abc11a067ead1c28e59e7d8e43ef7c049
Leonardo1124k <leonardopradopessoni@gmail.com>
2025-12-05 10:30:50 -0300

    Removendo pasta de arquivos python

--- Exercicios/Funcoes/Simples/desafio97.py
+++
@@ -1,8 +0,0 @@
-# Arquivo: desafio97
-def escreva(palavra):
-    quant = len(palavra) + 5
-    print("-" * quant)
-    print(f"{palavra:^{quant}}")
-    print("-" * quant)
-
-escreva(input("Escreva uma palavra: "))
