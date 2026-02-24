8c20a5f868838c9a738cf91cd6360cec87fab455
Leonardo1124k <leonardopradopessoni@gmail.com>
2026-02-24 16:26:37 -0300

    Vamos la

--- Abstract/src/application/Program.java
+++ Abstract/src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		List<Account> list = new ArrayList<>();
 		
-		list.add(new SavingsAccount(1001, "Alexandre", 500.00, 0.01));
+		list.add(new SavingsAccount(1001, "Alexandria", 500.00, 0.01));
 		list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
 		list.add(new SavingsAccount(1004, "Bob", 300.0, 0.01));
 		list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
